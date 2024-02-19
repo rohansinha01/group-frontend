fa1665509034ba5c81d8cf50b696a473e7e75b27
Daniel Ji <daniel.ji0923@gmail.com>
2024-02-18 17:14:32 -0800

    favicon and first styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+import './styles.scss';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import router from './router'
 import { RouterProvider } from 'react-router-dom';
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,26 +4,29 @@ const ShowAll = () => {
     const bookmarks = useLoaderData()
     console.log(bookmarks)
     return (
-        <div>
-            <h1>Add a Bookmark</h1>
-            <Form action={`/create`} method='post'>
-                <input type='input' name='name' placeholder="bookmark" />
-                <input type='input' name='url' placeholder="website" />
-                <input type='submit' value={`create bookmark`}/>
+        <div className="header">
+            <h1 className="nav-title">Add a Bookmark</h1>
+            <Form action={`/create`} method='post' className="add-form">
+                <input type='input' name='name' placeholder="bookmark" className="field" />
+                <input type='input' name='url' placeholder="website" className="field" />
+                <input type='submit' value={`create bookmark`} className="submit-button"/>
             </Form>
 
-            <h2>Bookmark List</h2>
-            {bookmarks.map(v => {
-                return (
-                    <div key={v._id} className="bookmark">
-                        <Link to={`/${v._id}`}>
-                            <h4>{v.name}</h4>
-                        </Link>
-                        
-                    </div>            
-                )
-            })}
-            
+            <div className="container">
+                <h2 className="bookmark-title">Bookmark List</h2>
+                <div className="bookmarks">
+                    {bookmarks.map(v => {
+                        return (
+                            <div key={v._id} className="bookmark">
+                                <Link className="links" to={`/${v._id}`}>
+                                    <h4>{v.name}</h4>
+                                </Link>
+                                
+                            </div>            
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
